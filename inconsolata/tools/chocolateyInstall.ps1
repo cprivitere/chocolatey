32d71adea0aadc5239b2cfd2cb4f5b1fb273c64a
Chris Privitere <cprivite@users.noreply.github.com>
2019-09-24 18:21:06 -0500

    Iniitial import after taking ownership.

---
+++ inconsolata/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿try 
+{
+    $packageName = 'inconsolata'
+    $url = 'http://www.fontsquirrel.com/fonts/download/Inconsolata'
+
+    $destination = Join-Path $Env:Temp $packageName
+
+    Install-ChocolateyZipPackage $packageName -url $url -unzipLocation $destination
+
+    $shell = New-Object -ComObject Shell.Application
+    $fontsFolder = $shell.Namespace(0x14)
+
+    Get-ChildItem $destination -Recurse -Filter *.otf |
+        ForEach-Object { $fontsFolder.CopyHere($_.FullName) }
+
+    Remove-Item $destination -Recurse
+} 
+catch 
+{
+    Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
+    throw
+}
