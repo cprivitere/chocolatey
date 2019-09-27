66810ae635bbd22633f937055b3a0eb129776a55
Chris Privitere <cprivite@users.noreply.github.com>
2019-09-26 21:08:07 -0500

    Add uninstall script, switch to using the chocolatey font helpers extension.

--- inconsolata/tools/chocolateyInstall.ps1
+++ inconsolata/tools/chocolateyInstall.ps1
@@ -1,35 +1,26 @@
-try
-{
-    $packageName = 'inconsolata'
-    $url1 = 'https://github.com/google/fonts/raw/master/ofl/inconsolata/Inconsolata-Regular.ttf'
-    $url2 = 'https://github.com/google/fonts/raw/master/ofl/inconsolata/Inconsolata-Bold.ttf'
+$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-    $destination = Join-Path $Env:Temp $packageName
+$url1 = 'https://github.com/google/fonts/raw/master/ofl/inconsolata/Inconsolata-Regular.ttf'
+$url2 = 'https://github.com/google/fonts/raw/master/ofl/inconsolata/Inconsolata-Bold.ttf'
 
-    Get-ChocolateyWebFile `
-      -PackageName $packageName `
-      -FileFullPath $destination\Inconsolata-Regular.ttf `
-      -Url $url1 `
-      -Checksum '2A53B53D55363C4913A8873D0E1636D6C09D8A3C38570FB166FC71A5123EC8DC' `
-      -ChecksumType SHA256 `
+Get-ChocolateyWebFile `
+  -PackageName $env:ChocolateyPackageName `
+  -FileFullPath $toolsDir\Inconsolata-Regular.ttf `
+  -Url $url1 `
+  -Checksum '2A53B53D55363C4913A8873D0E1636D6C09D8A3C38570FB166FC71A5123EC8DC' `
+  -ChecksumType SHA256 `
 
-    Get-ChocolateyWebFile `
-      -PackageName $packageName `
-      -FileFullPath $destination\Inconsolata-Bold.ttf `
-      -Url $url2 `
-      -Checksum '76A242626E1C791B6945060D7A61B5B61F6F1C6B364B6C7868A30AF1EF44B623' `
-      -ChecksumType SHA256 `
+Get-ChocolateyWebFile `
+  -PackageName $env:ChocolateyPackageName `
+  -FileFullPath $toolsDir\Inconsolata-Bold.ttf `
+  -Url $url2 `
+  -Checksum '76A242626E1C791B6945060D7A61B5B61F6F1C6B364B6C7868A30AF1EF44B623' `
+  -ChecksumType SHA256 `
 
-    $shell = New-Object -ComObject Shell.Application
-    $fontsFolder = $shell.Namespace(0x14)
+#Remove the old font if it's still kicking around
+Uninstall-ChocolateyFont Inconsolata.otf
 
-    Get-ChildItem $destination -Recurse -Filter *.ttf |
-        ForEach-Object { $fontsFolder.CopyHere($_.FullName) }
+Install-ChocolateyFont $toolsDir -multiple
 
-    Remove-Item $destination -Recurse
-}
-catch
-{
-    Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
-    throw
-}
+#Remove-Item $destination\Inconsolata-Regular.ttf $destination\Inconsolata-Bold.ttf
---
+++ inconsolata/tools/chocolateyUninstall.ps1
@@ -0,0 +1,3 @@
+$ErrorActionPreference = 'Stop'; # stop on all errors
+Uninstall-ChocolateyFont Inconsolata-Regular.ttf
+Uninstall-ChocolateyFont Inconsolata-Bold.ttf
