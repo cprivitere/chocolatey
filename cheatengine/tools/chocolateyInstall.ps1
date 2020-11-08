c3617437bab36db317496b82c9a4c746b1eff1f9
Chris Privitere <cprivite@users.noreply.github.com>
2020-11-08 14:49:24 -0600

    Updated to 7.2

--- cheatengine/tools/chocolateyInstall.ps1
+++ cheatengine/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 Install-ChocolateyPackage `
   -PackageName 'cheatengine' `
-  -Url 'https://github.com/cheat-engine/cheat-engine/releases/download/7.1/CheatEngine71.exe' `
-  -Checksum 'DF54B77363500D9795AB43D78A55989200B66974A2EAF90898FA9ACE240659E4' `
+  -Url 'https://github.com/cheat-engine/cheat-engine/releases/download/7.2/CheatEngine72.exe' `
+  -Checksum '3C1EAD7F3D812E2A3CF09C382ECC71212F5C86CE6A2DA7F4C7033925241AD53D' `
   -ChecksumType 'SHA256' `
   -FileType 'EXE' `
   -Silent '/VERYSILENT /NORESTART /NOCANDY'
